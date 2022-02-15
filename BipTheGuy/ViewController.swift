c9a89df1f20f88a1a7476eaed57c282e3ebd6d11
Pace Williams <pacewilliams32@gmail.com>
2022-02-15 14:29:01 -0500

    Initial Commit

---
+++ BipTheGuy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BipTheGuy
+//
+//  Created by Pace Williams on 2/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
